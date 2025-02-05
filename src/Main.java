fc44098acff77ae0396bcd418de1793255a92f31
Malin <malindqvist@outlook.com>
2025-02-05 20:56:48 +0100

    Added exception handling, translation functionality and test cases

--- src/Main.java
+++ src/Main.java
@@ -8,32 +8,65 @@ public class Main {
     public static void main(String[] args) {
         MorseCodeTranslator MCT = new MorseCodeTranslator();
         Scanner scanner = new Scanner(System.in);
+        String userInput;
+        boolean validChoice = false;
+        int choice = 0;
 
         System.out.println("Welcome to the Morse Code Translator");
-        System.out.println("What do you want to do?");
-        System.out.println("1. Translate from English to Morse");
-        System.out.println("2. Translate from Morse to English");
 
-        try {
-            int choice = scanner.nextInt();
-            while(choice != 1 && choice != 2){
-                System.out.println("Please enter 1. for English or 2. for Morse");
-                choice = scanner.nextInt();
-            }
-            scanner.nextLine();
-            System.out.println("Enter the text you wish to translate:");
-            if(choice == 1){
-                System.out.println("Translation:");
-                System.out.println(MCT.englishToMorse(scanner.nextLine()));
+        while (true) {
+            try {
+                System.out.println("What do you want to do?");
+                System.out.println("1. Translate from English to Morse Code");
+                System.out.println("2. Translate from Morse Code to English");
+
+                //Keeps asking for user input until valid
+                while (!validChoice) {
+                    try {
+                        choice = scanner.nextInt();
+                        if (choice == 1 || choice == 2) {
+                            validChoice = true;
+                        } else {
+                            System.out.println("Please enter 1. for English or 2. for Morse Code");
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Please enter 1. for English or 2. for Morse Code");
+                        //Clearing the scanner to avoid writing the error message for eternity
+                        scanner.nextLine();
+                    }
+                }
+
+                //Clearing the scanner
+                scanner.nextLine();
+
+                switch (choice) {
+                    case 1:
+                        System.out.println("Enter the text you want to translate:");
+                        System.out.println(MCT.englishToMorse(scanner.nextLine()));
+                        break;
+                    case 2:
+                        System.out.println("Enter the Morse Code you want to translate:");
+                        userInput = scanner.nextLine();
+                        //Checks the user input. Only dots, dashes and spaces are allowed
+                        if (userInput.matches("[.\\s\\-]+")) {
+                            System.out.println(MCT.morseToEnglish(userInput));
+                        } else {
+                            System.out.println("Invalid input. Only dots, dashes and spaces are allowed.");
+                        }
+
+                        break;
+                    default:
+                        System.out.println("Invalid input");
+                }
+            } catch (Exception e) {
+                System.out.println("Error: " + e);
             }
 
-            if (choice == 2) {
-                System.out.println("Translate to English");
-            }
 
-        } catch (Exception e){
-            System.out.println("Error: " + e);
         }
 
+
     }
+
+
 }
--- src/MorseCodeTranslatorTest.java
+++ src/MorseCodeTranslatorTest.java
@@ -6,11 +6,37 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class MorseCodeTranslatorTest {
+    MorseCodeTranslator mct = new MorseCodeTranslator();
+
     @Test
-    public void englishToMorseTest(){
-        MorseCodeTranslator mct = new MorseCodeTranslator();
-        String testText = "cat";
-        assertEquals("-.- .- \"-\" ", mct.englishToMorse(testText));
+    //English to Morse Code translation
+    public void englishToMorseTest() {
+        assertEquals("-.-. .- - ", mct.englishToMorse("cat"));
+    }
+
+    @Test
+    //Morse Code to English translation
+    public void morseToEnglishTest() {
+        assertEquals("cat", mct.morseToEnglish("-.-. .- -"));
+    }
+
+    @Test
+    //Empty input should return an empty string, not crashing
+    public void emptyInputTest() {
+        assertEquals("", mct.morseToEnglish(""));
+    }
+
+    @Test
+    //Extra spaces in the input text should be removed
+    public void morseToEnglishTextSplitTest() {
+        assertEquals("snail", mct.morseToEnglish("...  -.  .- ..  .-.."));
+    }
+
+    @Test
+    //The English to Morse translation should give the same result with uppercase or lowercase letters
+    public void englishToMorseCaseTest() {
+        assertEquals(".... . .-.. .-.. --- ", mct.englishToMorse("hello"));
+        assertEquals(".... . .-.. .-.. --- ", mct.englishToMorse("HELLO"));
     }
 
 }
